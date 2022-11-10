34d8b16498e6781bb47c4a5f4970fc32fc86386f
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-11-10 10:16:50 +0100

    WIP: reproducer test for the secondary supers cache issue

---
+++ tests/camel-jmh/src/test/java/org/apache/camel/itest/jmh/CreateExchangeTest.java
@@ -0,0 +1,75 @@
+package org.apache.camel.itest.jmh;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.ExtendedExchange;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.engine.PrototypeExchangeFactory;
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+public class CreateExchangeTest {
+
+    @Test
+    public void launchBenchmark() throws Exception {
+        Options opt = new OptionsBuilder()
+                // Specify which benchmarks to run.
+                // You can be more specific if you'd like to run only one benchmark per test.
+                .include(this.getClass().getName() + ".*")
+                // Set the following options as needed
+                .mode(Mode.All)
+                .timeUnit(TimeUnit.MICROSECONDS)
+                .warmupTime(TimeValue.seconds(1))
+                .warmupIterations(2)
+                .measurementTime(TimeValue.seconds(1))
+                .measurementIterations(2)
+                .threads(2)
+                .forks(1)
+                .shouldFailOnError(true)
+                .shouldDoGC(true)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    // The JMH samples are the best documentation for how to use it
+    // http://hg.openjdk.java.net/code-tools/jmh/file/tip/jmh-samples/src/main/java/org/openjdk/jmh/samples/
+    @State(Scope.Thread)
+    public static class BenchmarkState {
+        CamelContext context;
+        PrototypeExchangeFactory factory;
+
+        @Setup(Level.Trial)
+        public void initialize() {
+             context = new DefaultCamelContext();
+             factory = new PrototypeExchangeFactory();
+
+            factory.setCamelContext(context);
+        }
+    }
+
+
+    @Benchmark
+    @Measurement(batchSize = 1000000)
+    public void benchmark(CreateExchangeTest.BenchmarkState state, Blackhole bh) {
+        final Exchange exchange = state.factory.create(true);
+
+        exchange.adapt(ExtendedExchange.class).setFromRouteId("Something");
+        bh.consume(exchange);
+    }
+
+
+}
