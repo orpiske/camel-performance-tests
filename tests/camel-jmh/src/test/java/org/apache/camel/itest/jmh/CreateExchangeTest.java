497d073f82cec4f397035e400b2ac338e443f091
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-11-11 11:05:34 +0100

    WIP: more environment adjustments

--- tests/camel-jmh/src/test/java/org/apache/camel/itest/jmh/CreateExchangeTest.java
+++ tests/camel-jmh/src/test/java/org/apache/camel/itest/jmh/CreateExchangeTest.java
@@ -4,9 +4,11 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.ExtendedCamelContext;
 import org.apache.camel.ExtendedExchange;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.engine.PrototypeExchangeFactory;
+import org.apache.camel.model.ModelCamelContext;
 import org.apache.camel.support.DefaultExchange;
 import org.junit.jupiter.api.Test;
 import org.openjdk.jmh.annotations.Benchmark;
@@ -34,10 +36,8 @@ public class CreateExchangeTest {
                 // Set the following options as needed
                 .mode(Mode.SingleShotTime)
                 .timeUnit(TimeUnit.MICROSECONDS)
-//                .warmupTime(TimeValue.seconds(1))
-//                .warmupIterations(2)
-//                .measurementTime(TimeValue.seconds(15))
-                .measurementIterations(5)
+                .measurementIterations(20)
+                .warmupIterations(5)
                 .threads(Runtime.getRuntime().availableProcessors())
                 .forks(1)
                 .build();
@@ -49,61 +49,68 @@ public class CreateExchangeTest {
     // http://hg.openjdk.java.net/code-tools/jmh/file/tip/jmh-samples/src/main/java/org/openjdk/jmh/samples/
     @State(Scope.Benchmark)
     public static class BenchmarkState {
+
         CamelContext context;
         PrototypeExchangeFactory factory;
+        ExtendedCamelContext extendedCamelContext;
+        ModelCamelContext modelCamelContext;
 
         @Setup(Level.Trial)
         public void initialize() {
             context = new DefaultCamelContext();
             factory = new PrototypeExchangeFactory();
+        }
+    }
 
-            factory.setCamelContext(context);
+    // Proposed solution
+    @Benchmark
+    @Measurement(batchSize = 50000)
+    public void benchmarkWithCachedContext(CreateExchangeTest.BenchmarkState state, Blackhole bh) {
+        if (state.extendedCamelContext == null) {
+            state.extendedCamelContext = state.context.adapt(ExtendedCamelContext.class);
+        }
+
+        if (state.modelCamelContext == null) {
+            state.modelCamelContext = state.context.adapt(ModelCamelContext.class);
+        }
+
+        state.extendedCamelContext.setDescription("extended");
+        state.modelCamelContext.setManagementName("model");
+
+        bh.consume(state.extendedCamelContext);
+        bh.consume(state.modelCamelContext);
+    }
+
+    // What may happen in unhappy paths
+    @Benchmark
+    @Measurement(batchSize = 50000)
+    public void benchmarkWithInstanceOfAndAdapt(CreateExchangeTest.BenchmarkState state, Blackhole bh) {
+        if (state.context instanceof ExtendedCamelContext) {
+            final ExtendedCamelContext extendedCamelContext = state.context.adapt(ExtendedCamelContext.class);
+            extendedCamelContext.setDescription("extended");
+            bh.consume(extendedCamelContext);
+        }
+
+        if (state.context instanceof ModelCamelContext) {
+            final ModelCamelContext modelCamelContext = state.context.adapt(ModelCamelContext.class);
+            modelCamelContext.setManagementName("model");
+            bh.consume(modelCamelContext);
         }
     }
 
 
+    // More closely represents the happy path in many of the Camel code
     @Benchmark
-    @Measurement(batchSize = 5000)
-    public void benchmarkWithExchangeAndAdaptSet(CreateExchangeTest.BenchmarkState state, Blackhole bh) {
-        final Exchange exchange = state.factory.create(true);
+    @Measurement(batchSize = 50000)
+    public void benchmarkWithNoInstanceOfAndAdapt(CreateExchangeTest.BenchmarkState state, Blackhole bh) {
+        final ExtendedCamelContext extendedCamelContext = state.context.adapt(ExtendedCamelContext.class);
+        extendedCamelContext.setDescription("extended");
+        bh.consume(extendedCamelContext);
 
-        exchange.adapt(ExtendedExchange.class).setFromRouteId("Something");
-        bh.consume(exchange);
-    }
+        final ModelCamelContext modelCamelContext = state.context.adapt(ModelCamelContext.class);
+        modelCamelContext.setManagementName("model");
+        bh.consume(modelCamelContext);
 
-    @Benchmark
-    @Measurement(batchSize = 5000)
-    public void benchmarkWithExchangeAndSet(CreateExchangeTest.BenchmarkState state, Blackhole bh) {
-        final Exchange exchange = state.factory.create(true);
-
-        exchange.setExchangeId("lalala");
-        bh.consume(exchange);
-    }
-
-    @Benchmark
-    @Measurement(batchSize = 5000)
-    public void benchmarkWithExchangeNoSet(CreateExchangeTest.BenchmarkState state, Blackhole bh) {
-        final Exchange exchange = state.factory.create(true);
-
-        bh.consume(exchange);
-    }
-
-    @Benchmark
-    @Measurement(batchSize = 5000)
-    public void benchmarkWithDefaultExchangeAndAdaptSet(CreateExchangeTest.BenchmarkState state, Blackhole bh) {
-        final DefaultExchange exchange = new DefaultExchange(state.context);
-
-        exchange.adapt(ExtendedExchange.class).setFromRouteId("Something");
-        bh.consume(exchange);
-    }
-
-    @Benchmark
-    @Measurement(batchSize = 5000)
-    public void benchmarkWithDefaultExchangeAndSet(CreateExchangeTest.BenchmarkState state, Blackhole bh) {
-        final DefaultExchange exchange = new DefaultExchange(state.context);
-
-        exchange.setFromRouteId("Something");
-        bh.consume(exchange);
     }
 
 }
